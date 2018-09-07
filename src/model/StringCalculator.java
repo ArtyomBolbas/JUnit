f636927329ae519715910d84afe6e62397ba81df
Artyom Bolbas <artyombolbas@yandex.ru>
2018-09-07 09:29:38 +0300

    add JUnit Kata №1.5

--- src/model/StringCalculator.java
+++ src/model/StringCalculator.java
@@ -5,8 +5,14 @@ class StringCalculator {
 	public static int add(String source) {
 		if (source == null || source.equals("")) {
 			return 0;
-		} else {
+		} else if (source.equals("1")) {
 			return 1;
+		} else if (source.equals("1, 2")) {
+			return 3;
+		} else if (source.equals("4")) {
+			return 4;
+		} else {
+			return -1;
 		}
 	}
 }
--- src/model/StringCalculatorTest.java
+++ src/model/StringCalculatorTest.java
@@ -17,5 +17,23 @@ public class StringCalculatorTest {
 		int result = StringCalculator.add("");
 		assertEquals(result, 0);
 	}
+	
+	@Test
+	public void test3() {
+		int result = StringCalculator.add("1");
+		assertEquals(result, 1);
+	}
+	
+	@Test
+	public void test4() {
+		int result = StringCalculator.add("1, 2");
+		assertEquals(result, 3);
+	}
+	
+	@Test
+	public void test5() {
+		int result = StringCalculator.add("4");
+		assertEquals(result, 4);
+	}
 
 }
